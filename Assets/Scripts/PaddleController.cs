8116dbfb8d0d775056ff71a59e0fae70fbae8770
basileia <jane.novak@centrum.cz>
2026-07-21 21:20:26 +0200

    touch for tablet

--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -20,12 +20,39 @@ public class PaddleController : MonoBehaviour
     }
     private void Update()
     {
-        lastY = transform.position.y;
+        float lastY = transform.position.y;
 
+        // klávesnice
         float input = Input.GetAxisRaw("Vertical");
+        if (input != 0)
+        {
+            Move(input);
+        }
 
-        Move(input);
+        // Tablet / mobil (touch)
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
 
+            if (touch.position.x < Screen.width * 0.5f) // jen levá polovina obrazovky
+            {
+                Vector3 worldPos = Camera.main.ScreenToWorldPoint(touch.position);
+                MoveTo(worldPos.y);
+            }
+        }
+
+        // Myš
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (Input.mousePosition.x < Screen.width * 0.5f)
+            {
+                MoveTo(worldPos.y);
+            }
+        }
+
+        // spočítáme rychlost paddle (pro spin)
         paddleVelocity = (transform.position.y - lastY) / Time.deltaTime;
     }
 
@@ -37,5 +64,14 @@ public class PaddleController : MonoBehaviour
         newPosition.y = Mathf.Clamp(newPosition.y, minY, maxY);
 
         transform.position = newPosition;
+    }
+
+    private void MoveTo(float targetY)
+    {
+        float clampedY = Mathf.Clamp(targetY, minY, maxY);
+
+        Vector3 pos = transform.position;
+        pos.y = Mathf.Lerp(pos.y, clampedY, Time.deltaTime * 10f);
+        transform.position = pos;
     }
 }
