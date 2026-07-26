72ef064bf8e8732e023fe528031d2726045eed29
basileia <jane.novak@centrum.cz>
2026-07-26 14:42:29 +0200

    new panel

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -1,14 +1,32 @@
 using UnityEngine;
 
+public enum GoalSide
+{
+    Left,
+    Right
+}
+
 public class Goal : MonoBehaviour
 {
     [SerializeField] private BallController ball;
+    [SerializeField] private ScoreManager scoreManager;
+    [SerializeField] private bool isLeftGoal;   // true = levý gól, false = pravý gól
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Ball"))
         {
+            if (isLeftGoal)
+            {
+                scoreManager.AddRightScore();
+            }
+            else
+            {
+                scoreManager.AddLeftScore();
+            }
+
             ball.ResetBall();
         }
     }
 }
+
